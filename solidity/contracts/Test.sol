d8ab00de8ca500c24b415d6bf1ed40cb3d6f8d6e
wangkui0508 <wangkui0508@hotmail.com>
2022-01-25 08:47:20 +0800

    Update Test.sol

--- solidity/contracts/Test.sol
+++ solidity/contracts/Test.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.10;
 
 contract Test {
-	function func0() external {
-		address(this).call{gas: 9000}("");
+	function func0(uint gas) external {
+		address(this).call{gas: gas}(abi.encodeWithSignature("func1"));
 	}
 	function func1(address addr) external {
 	}
